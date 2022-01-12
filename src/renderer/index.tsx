b5edce6fa10c9e5a17910f3cd3d2ca61c26c79c2
unknown <andreyxdd@yandex.ru>
2022-01-11 22:14:32 -0700

    Checking what ubuntu test says - 2

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,4 +1,10 @@
 import { render } from 'react-dom';
 import App from './App';
 
+declare global {
+  interface Window {
+    electron: any; // eslint-disable-line
+  }
+}
+
 render(<App />, document.getElementById('root'));
