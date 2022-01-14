17207b52db5fd6837222f5e8539f21904d70c016
unknown <andreyxdd@yandex.ru>
2022-01-14 16:22:54 -0700

    Implemented new functional in the DataTable component: the columns are hidden for the specific data-types
    Updated the code implementation: separated the defaul definition of the columns and added a new state to the component

---
+++ src/renderer/components/DataTable/columnsDefenition.tsx
@@ -0,0 +1,447 @@
+import { GridColDef, GridValueFormatterParams } from '@mui/x-data-grid';
+
+export const columnsDefinition: GridColDef[] = [
+  {
+    field: 'id',
+    headerName: '#',
+    width: 10,
+    align: 'center',
+    headerAlign: 'center',
+    description: 'Ranking for the given creteria',
+  },
+  {
+    field: 'ticker',
+    headerName: 'Ticker',
+    width: 80,
+    renderCell: (params) => (
+      <a
+        href={`https://finance.yahoo.com/quote/${params.value}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        {params.value}
+      </a>
+    ),
+    align: 'center',
+    headerAlign: 'center',
+    description: 'Stock symbol',
+  },
+  {
+    field: 'macd',
+    headerName: 'MACD',
+    type: 'number',
+    width: 80,
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      return (params.value as number).toPrecision(4);
+    },
+    description:
+      'Moving average convergence divergence. It is calculated by subtracting the 26-period EMA from the 12-period EMA',
+  },
+  {
+    field: 'one_day_avg_mf',
+    headerName: '1-day Money Flow, $',
+    width: 200,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number).toFixed(2)
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description: 'Money flow averaged over 1 day using typical price',
+  },
+  {
+    field: 'three_day_avg_mf',
+    headerName: '3-day Avg Money Flow, $',
+    width: 200,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number).toFixed(2)
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description:
+      'Money flow averaged over 3 days based on 1-day average money flows',
+  },
+  {
+    field: 'one_day_open_close_change',
+    headerName: '1-day Open-Close Change, %',
+    width: 220,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number) * 100
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description: 'Precentage change between open and close prices',
+  },
+  {
+    field: 'volume',
+    headerName: 'Volume',
+    width: 150,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number).toFixed(2)
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description: 'Stock volume',
+  },
+  {
+    field: 'three_day_avg_volume',
+    headerName: '3-day Avg Volume',
+    width: 150,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number).toFixed(2)
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description: 'Stock volume averaged over three days',
+  },
+  {
+    field: 'one_day_volume_change',
+    headerName: '1-day Volume Change, %',
+    width: 220,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number) * 100
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description:
+      'Precentage change between closing volumes for the given trading day and the day before',
+  },
+  {
+    field: 'three_day_avg_volume_change',
+    headerName: '3-day Avg Volume Change, %',
+    width: 220,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number) * 100
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description:
+      'Precentage change between closing volumes for the given trading day and the day before averaged over three days',
+  },
+  {
+    field: 'one_day_close_change',
+    headerName: '1-day Close Change, %',
+    width: 220,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number) * 100
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description:
+      'Precentage change between closing prices for the given trading day and the day before',
+  },
+  {
+    field: 'three_day_avg_close_change',
+    headerName: '3-day Avg Close Change, %',
+    width: 220,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number) * 100
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description:
+      'Precentage change between closing prices for the given trading day and the day before averaged over three days',
+  },
+  {
+    field: 'ema_3over9',
+    headerName: 'EMA 3/9',
+    width: 100,
+    type: 'string',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      return (params.value as Array<string>).join('-');
+    },
+    description: `Comparison between 3-day EMA and 9-day EMA. 'A' is above, 'B' is below. Three letters shows comparison for three last trading periods; one letter only for one trading period.`,
+  },
+  {
+    field: 'ema_12over9',
+    headerName: 'EMA 12/9',
+    width: 100,
+    type: 'string',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      return (params.value as Array<string>).join('-');
+    },
+    description: `Comparison between 12-day EMA and 9-day EMA. 'A' is above, 'B' is below. Three letters shows comparison for three last trading periods; one letter only for one trading period.`,
+  },
+  {
+    field: 'ema_12over26',
+    headerName: 'EMA 12/26',
+    width: 100,
+    type: 'string',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      return (params.value as Array<string>).join('-');
+    },
+    description: `Comparison between 12-day EMA and 26-day EMA. 'A' is above, 'B' is below. Three letters shows comparison for three last trading periods; one letter only for one trading period.`,
+  },
+  {
+    field: 'ema_50over20',
+    headerName: 'EMA 50/20',
+    width: 100,
+    type: 'string',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      return (params.value as Array<string>).join('-');
+    },
+    description: `Comparison between 50-day EMA and 20-day EMA. 'A' is above, 'B' is below. Three letters shows comparison for three last trading periods; one letter only for one trading period.`,
+  },
+  {
+    field: 'closingPriceChangeDay12',
+    headerName: 'Days 1-2 Close Change, %',
+    width: 200,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number) * 100
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description:
+      'Precentage change between closing prices for the given trading day and the day before',
+  },
+  {
+    field: 'closingPriceChangeDay23',
+    headerName: 'Days 2-3 Close Change, %',
+    width: 200,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number) * 100
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description:
+      'Precentage change between closing prices for the trading day before given date and the day before that',
+  },
+  {
+    field: 'mfi',
+    headerName: 'Money Flow Index',
+    width: 150,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number).toPrecision(4)
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description:
+      'Money Flow Index - is a technical oscillator that uses price and volume data for identifying overbought or oversold signals in an asset. It can also be used to spot divergences which warn of a trend change in price. The oscillator moves between 0 and 100.',
+  },
+  {
+    field: 'ema3',
+    headerName: 'EMA3',
+    width: 100,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number).toPrecision(4)
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description:
+      'Exponential Moving Average for the period of three days from the given date (including)',
+  },
+  {
+    field: 'ema9',
+    headerName: 'EMA9',
+    width: 100,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number).toPrecision(4)
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description:
+      'Exponential Moving Average for the period of nine days from the given date (including)',
+  },
+  {
+    field: 'ema12',
+    headerName: 'EMA12',
+    width: 100,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number).toPrecision(4)
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description:
+      'Exponential Moving Average for the period of 12 days from the given date (including)',
+  },
+  {
+    field: 'ema20',
+    headerName: 'EMA20',
+    width: 100,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number).toPrecision(4)
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description:
+      'Exponential Moving Average for the period of 20 days from the given date (including)',
+  },
+  {
+    field: 'ema26',
+    headerName: 'EMA26',
+    width: 100,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number).toPrecision(4)
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description:
+      'Exponential Moving Average for the period of 26 days from the given date (including)',
+  },
+  {
+    field: 'ema50',
+    headerName: 'EMA50',
+    width: 100,
+    type: 'number',
+    align: 'center',
+    headerAlign: 'center',
+    valueFormatter: (params: GridValueFormatterParams) => {
+      const valueFormatted = Number(
+        (params.value as number).toPrecision(4)
+      ).toLocaleString();
+      return valueFormatted;
+    },
+    description:
+      'Exponential Moving Average for the period of 50 days from the given date (including)',
+  },
+];
+
+interface IColumnsToShowProps {
+  by_one_day_avg_mf: Array<string>;
+  by_three_day_avg_mf: Array<string>;
+  by_five_prec_open_close_change: Array<string>;
+  by_volume: Array<string>;
+  by_three_day_avg_volume: Array<string>;
+  [key: string]: Array<string>;
+}
+
+export const columnsToShow: IColumnsToShowProps = {
+  by_one_day_avg_mf: [
+    'id',
+    'ticker',
+    'macd',
+    'ema_3over9',
+    'ema_12over9',
+    'ema_12over26',
+    'ema_50over20',
+    'one_day_volume_change',
+    'one_day_open_close_change',
+    'one_day_avg_mf',
+    'mfi',
+  ],
+  by_three_day_avg_mf: [
+    'id',
+    'ticker',
+    'macd',
+    'ema_3over9',
+    'ema_12over9',
+    'ema_12over26',
+    'ema_50over20',
+    'three_day_avg_volume_change',
+    'three_day_avg_close_change',
+    'closingPriceChangeDay12',
+    'closingPriceChangeDay23',
+    'three_day_avg_mf',
+    'mfi',
+  ],
+  by_five_prec_open_close_change: [
+    'id',
+    'ticker',
+    'macd',
+    'ema_3over9',
+    'ema_12over9',
+    'ema_12over26',
+    'one_day_open_close_change',
+    'one_day_avg_mf',
+    'mfi',
+  ],
+  by_volume: [
+    'id',
+    'ticker',
+    'macd',
+    'ema_3over9',
+    'ema_12over9',
+    'ema_12over26',
+    'one_day_open_close_change',
+    'one_day_avg_mf',
+    'mfi',
+  ],
+  by_three_day_avg_volume: [
+    'id',
+    'ticker',
+    'macd',
+    'ema_3over9',
+    'ema_12over9',
+    'ema_12over26',
+    'one_day_open_close_change',
+    'three_day_avg_mf',
+    'mfi',
+  ],
+};
--- src/renderer/components/DataTable/dataProcessing.tsx
+++ src/renderer/components/DataTable/dataProcessing.tsx
@@ -1,22 +1,22 @@
 import { IDataProps } from '../../../types';
 
 const processData = (dataList: Array<IDataProps>, dataType: string) => {
-  dataList.forEach((data: IDataProps, idx: number) => {
-    Object.keys(data).forEach((key: string) => {
-      if (
-        [
-          'by_one_day_avg_mf',
-          'by_five_prec_open_close_change',
-          'by_volume',
-        ].includes(dataType) &&
-        ['ema_3over9', 'ema_12over9', 'ema_12over26', 'ema_50over20'].includes(
-          key
-        )
-      ) {
-        data[key] = (data[key] as Array<string>).slice(0, 1);
-      }
-    });
-    data.id = idx + 1;
+  return dataList.map((data: IDataProps, idx: number) => {
+    const updateData = { ...data, id: idx + 1 };
+    if (
+      [
+        'by_one_day_avg_mf',
+        'by_five_prec_open_close_change',
+        'by_volume',
+      ].includes(dataType)
+    ) {
+      updateData.ema_3over9 = data.ema_3over9.slice(0, 1);
+      updateData.ema_12over9 = data.ema_12over9.slice(0, 1);
+      updateData.ema_12over26 = data.ema_12over26.slice(0, 1);
+      updateData.ema_50over20 = data.ema_50over20.slice(0, 1);
+    }
+
+    return updateData;
   });
 };
 
